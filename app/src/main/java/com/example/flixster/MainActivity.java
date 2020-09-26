425054d1810b1a9e352ea79fc2cc89638284b733
Rennah Weng <rennahweng628@gmail.com>
2020-09-26 00:52:34 -0400

    Got list of movies from http response json

--- app/src/main/java/com/example/flixster/MainActivity.java
+++ app/src/main/java/com/example/flixster/MainActivity.java
@@ -6,10 +6,13 @@ import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.models.Movie;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String TAG = "MainActivity";
+
+    List<Movie> movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results: " + results.toString());
+                    movieList = Movie.moviesFromJsonArray(results);
+
                 } catch (JSONException e) {
                     // parse key might not exist or other issue when parsing json
                     Log.e(TAG, "Hit json exception", e);  // exceptions are all Throwable object
--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -1,7 +1,13 @@
 package com.example.flixster.models;
 
+import com.facebook.stetho.inspector.jsonrpc.JsonRpcException;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 // This class encapsulate the idea of an Movie
 public class Movie {
@@ -15,6 +21,15 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+    }
+
+    // Return a list of movies from the jsonArray "results"
+    public static List<Movie> moviesFromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movieList = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movieList.add( new Movie( movieJsonArray.getJSONObject(i) ) );
+        }
+        return movieList;
     }
 
 }
